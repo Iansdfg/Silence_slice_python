29509ccb58f307825dfcc99eb2334dadf04c7b6d
fengtxy <feng.txy@outlook.com>
2019-11-08 18:46:54 -0800

    add all file

---
+++ audio_slice.py
@@ -0,0 +1,42 @@
+# Import the AudioSegment class for processing audio and the 
+# split_on_silence function for separating out silent chunks.
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+# Define a function to normalize a chunk to a target amplitude.
+def match_target_amplitude(aChunk, target_dBFS):
+    change_in_dBFS = target_dBFS - aChunk.dBFS
+    return aChunk.apply_gain(change_in_dBFS)
+
+# Load your audio.
+song = AudioSegment.from_wav("001_S_T.wav")
+dBFS = song.dBFS
+# Split track where the silence is 2 seconds or more and get chunks using 
+# the imported function.
+chunks = split_on_silence (
+    # Use the loaded audio.
+    song, 
+    # Specify that a silent chunk must be at least 2 seconds or 2000 ms long.
+    min_silence_len = 500,
+    # Consider a chunk silent if it's quieter than -16 dBFS.
+    # (You may want to adjust this parameter.)
+    silence_thresh = dBFS-10
+)
+
+# Process each chunk with your parameters
+for i, chunk in enumerate(chunks):
+    # Create a silence chunk that's 0.5 seconds (or 500 ms) long for padding.
+    silence_chunk = AudioSegment.silent(duration=500)
+
+    # Add the padding chunk to beginning and end of the entire chunk.
+    audio_chunk = silence_chunk + chunk + silence_chunk
+
+    # Normalize the entire chunk.
+    normalized_chunk = match_target_amplitude(audio_chunk, -20.0)  
+    # Export the audio chunk with new bitrate.
+    print("Exporting chunk{0}.mp3.".format(i))
+    normalized_chunk.export(
+        ".//chunk{0}.wav".format(i),
+        bitrate = "192k",
+        format = "wav"
+    )
